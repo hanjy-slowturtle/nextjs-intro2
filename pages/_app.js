2f2e50f7cf7c5ebd2568949e6ba3f0d26e2336aa
hanjy-slowturtle <h920915@gmail.com>
2022-06-15 23:06:48 +0900

    ~ #1.6 Custom App

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import NavBar from "../components/NavBar";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        a {
+          color: white;
+        }
+
+        .active {
+          color: red;
+        }
+      `}</style>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
-import NavBar from "./components/NavBar";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1 className="active">Hello</h1>
-      <style jsx>{`
-        a {
-          color: white;
-        }
-
-        .active {
-          color: red;
-        }
-      `}</style>
     </div>
   );
 }
